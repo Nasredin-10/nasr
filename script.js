45266e5b3c097f8b6503b8af086b4651b07c5fa6
alimon05000 <alimsrifrv23@gmail.com>
2025-07-30 23:24:27 +0300

    Update and rename package.json to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD...",
+  authDomain: "game-649c1.firebaseapp.com",
+  databaseURL: "https://game-649c1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "game-649c1",
+  storageBucket: "game-649c1.appspot.com",
+  messagingSenderId: "177082622088",
+  appId: "1:177082622088:web:2d522421fe38a3dda70665"
+};
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
